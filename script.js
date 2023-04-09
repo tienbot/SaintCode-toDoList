b30aa80d4d1f09e20b3198e8455990c33fb3f184
tienbot1 <tien.ru@mail.ru>
2023-04-09 23:28:50 +0900

    delete CloseItemsBtn && deleteAllItemsBtn are works

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const input = document.querySelector('#input')
 const toDoListArr = []
 const checkbox = document.querySelectorAll('.customCheckbox')
 const delleteItemBtn = document.querySelectorAll('.deleteItem')
-const closeItem = document.querySelector('.close')
 const deleteCloseItemsBtn = document.querySelector('#deleteClose')
+const deleteAllItemsBtn = document.querySelector('#deleteAll')
+let closeItem = ''
+let lists = document.querySelectorAll('.list')
+const toDoList__main = document.querySelector('.toDoList__main')
+const toDoList__bottom = document.querySelector('.toDoList__bottom')
 
 //убрать дефолтное поведение формы (убрать обновление страницы)
 form.addEventListener("submit", (e)=>{
@@ -17,24 +21,6 @@ input.addEventListener('change', (event) => {
     window.localStorage.setItem('toDoList', toDoListArr)
     console.log(input.value)
 })
-
-//перечеркнуть задание при нажатии
-function isCheck(item) {
-    item.nextElementSibling.classList.toggle('close')
-}
-
-//удалить задание при нажатии
-function deleteItem(item) {
-    item.parentElement.style.display='none'
-}
-
-//удалить все завершенные задания
-function deleteCloseItems(item) {
-    // item.forEach(el => {
-    //     console.log(el)
-    // })
-    console.log(item)
-}
 
 //перебор всех кнопок удаления заданий
 delleteItemBtn.forEach(el => {
@@ -50,18 +36,49 @@ checkbox.forEach(el => {
     })
 })
 
-//перебор всех завершенных заданий
-// closeItem.forEach(el => {
-//     console.log(el.parentElement.parentElement)
-// })
-
+//запустить удаление всех завершенные заданий при нажатии
 deleteCloseItemsBtn.addEventListener('click', () => {
     deleteCloseItems(closeItem)
 })
 
-////вводим в инпут - добавляем в localStorage, оттуда в инпуты
+//запустить удаление всех заданий при нажатии
+deleteAllItemsBtn.addEventListener('click', () => {
+    deleteAllItems(lists)
+})
+
+//перечеркнуть задание при нажатии
+function isCheck(item) {
+    item.nextElementSibling.classList.toggle('close')
+}
+
+//удалить задание при нажатии
+function deleteItem(item) {
+    item.parentElement.style.display='none'
+}
+
+//удалить все завершенные задания
+function deleteCloseItems(item) {
+    item = document.querySelectorAll('.close')
+    item.forEach(el => {
+        el.parentElement.parentElement.style.display='none'
+    })
+}
+
+//удалить все задания
+function deleteAllItems(item) {
+    item.forEach(el => {
+        el.style.display='none'
+    })
+    toDoList__main.style.display='none'
+    toDoList__bottom.style.display='none'
+}
+
+//Отрисовка чекбокса через JS
 //Добавить - добавить чекбокс после нажатия кнопки
-//Удалить завершенные - удалить перечеркнутые
-//Удалить все - удалить все = очистить localStorage
+//вводим в инпут - добавляем в localStorage, оттуда в инпуты
+//Удалить все - очистить localStorage
 //удалить чекбокс = удалить из localStorage
+
+//+ Удалить завершенные - удалить перечеркнутые
+//+ Удалить все - удалить все = очистить localStorage
 
