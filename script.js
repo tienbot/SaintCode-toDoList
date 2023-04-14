8ea586fe71c2f96f0c2c6a995cbf968ef463f02e
tienbot1 <tien.ru@mail.ru>
2023-04-15 03:01:49 +0900

    delCloseItems is work

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let toDoListArr = [] //массиваная версия LS
+let toDoListArr = [] //массиваная версия LS (массивLS)
 const toDoList__main = document.querySelector('.toDoList__main')
 const toDoList__bottom = document.querySelector('.toDoList__bottom')
 let closeItem = ''
 let i=1
-let localItems = null;//пустой LS
+let localItems = null; //пустой LS
+let checkboxChoose = []
 
 empty()
 
@@ -14,7 +15,7 @@ document.querySelector('form').addEventListener("submit", (e)=>{
 
 //проверка LS на пустоту
 if (window.localStorage.getItem('toDoList') !== null){
-    localItems = window.localStorage.getItem('toDoList')
+    localItems = window.localStorage.getItem('toDoList') //
     toDoListArr = localItems.split(',')  //создаем массив, разделенный запятыми
     toDoListArr.map((el) => { //перебор всех заданий из localStorage и отображение их на странице
         createItem(el, i)
@@ -37,6 +38,17 @@ function delLS(item){
     }
 }
 
+//удалить все задания
+function deleteAllItems() {
+    toDoListArr = [] //опустошаем массивLS
+    window.localStorage.removeItem('toDoList') //удаляем все значения toDoList из LS
+    item = document.querySelectorAll('.list') //находим все задания в списке и...
+    item.forEach(el => {
+        el.style.display='none' //... скрываем их
+    })
+    empty() //скрыть центральную и нижнюю часть блока
+}
+
 //отобразить задание
 function createItem(object, iter) {
     const toDoList__main = document.body.querySelector(".toDoList__main") //находим .toDoList__main
@@ -49,8 +61,12 @@ function createItem(object, iter) {
     deleteItem.innerHTML = '❌' //внутри рисуем крест
     //Слушатель 'X'
     deleteItem.addEventListener('click', () => {
-        deleteItem.parentElement.style.display='none'
-        delLS(deleteItem.parentElement.children[0].children[1].innerHTML)
+        if(toDoListArr.length == 1){
+            deleteAllItems()
+        } else {
+            deleteItem.parentElement.style.display='none'
+            delLS(deleteItem.parentElement.children[0].children[1].innerHTML)
+        }
     })
 
     const checkbox = document.createElement('input') //добавляем чекбокс
@@ -60,7 +76,15 @@ function createItem(object, iter) {
     checkbox.name = `toDo${iter}` //добавляем name
     //Слушатель 'checkbox'
     checkbox.addEventListener('click', () => {
-        checkbox.nextElementSibling.classList.toggle('close') //При нажатии добавляем класс close
+        checkbox.nextElementSibling.classList.toggle('close')
+         //При нажатии добавляем класс close
+        if(checkbox.nextSibling.classList == 'close'){ //если у элемента есть класс close
+            checkboxChoose.splice(0, 0, checkbox.nextSibling.innerHTML) //удаляем его из массива checkboxChoose
+            console.log(checkboxChoose)
+        } else {
+            checkboxChoose.splice(checkboxChoose.indexOf(checkbox.nextSibling.innerHTML), 1) //иначе - добавляем в массив checkboxChoose
+            console.log(checkboxChoose)
+        }
     })
 
     const label = document.createElement('label') //добавляем лейбл
@@ -75,10 +99,21 @@ function createItem(object, iter) {
     toDoList__bottom.style.display='flex'
 }
 
+//сравнить два массива и вернуть один с уникальными значениями
+function delDublicat(a, b) {
+    var result = [];
+    for(var i = 0; i < a.length; i++) {
+        if (b.indexOf(a[i]) == -1) {
+            result.push(a[i]);
+        }
+    }
+    return result;
+}
+
 // Слушатель input. Настройка работы input-а
 document.querySelector('#input').addEventListener('change', (event) => {
-    toDoListArr.push(input.value) //добавляем в массив значение из инпута
-    localItems = window.localStorage.setItem('toDoList', toDoListArr) //добавляем массив в LS
+    toDoListArr.push(input.value) //добавляем в массивLS значение из инпута
+    localItems = window.localStorage.setItem('toDoList', toDoListArr) //переносим данные из массиваLS в сам LS
     createItem(input.value, i) //отображаем элемент на странице; i - индекс для id
     i++ //добавляем i в счетчик
     event.target.value = '' //опустошаем input
@@ -86,24 +121,24 @@ document.querySelector('#input').addEventListener('change', (event) => {
 
 //Слушатель 'Удалить завершенные'
 document.querySelector('#deleteClose').addEventListener('click', () => {
-    closeItem = document.querySelectorAll('.close')
+    closeItem = document.querySelectorAll('.close') //перебираем все элементы с классом close...
     closeItem.forEach(el => {
-        el.parentElement.parentElement.style.display='none'
+        el.parentElement.parentElement.style.display='none' //... скрываем все, что нашли 
     })
-    delLS()
+    toDoListArr = delDublicat(toDoListArr, checkboxChoose) //сравниваем массив завершенных дел и массивLS
+    localItems = window.localStorage.setItem('toDoList', toDoListArr) //переносим данные из массиваLS в сам LS
+    checkboxChoose = [] //обнуляем массив выбранных элементов
+
+    if(toDoListArr.length == 0){ //если массивLS пустой...
+        deleteAllItems()//удаляем все и скрываем середину и низ блока
+    }
 })
 
 //Слушатель 'Удалить все'
 document.querySelector('#deleteAll').addEventListener('click', () => {
-    toDoListArr = [] //массивная версия LS теперь пуста
-    window.localStorage.removeItem('toDoList') //удаляем значение toDoList из LS
-    item = document.querySelectorAll('.list') //находим все задания в списке и...
-    item.forEach(el => {
-        el.style.display='none' //... скрываем их
-    })
-    empty() //скрыть центральную и нижнюю часть блока
+    deleteAllItems()
 })
 
 // минусы:
-// - нажимаем на удаление - удаляется последний элемент
-// удалить завершенные
+//в LS должен быть контент и состояние. массив из объектов!
+//обновить страницу - завершенные дела сохранены
