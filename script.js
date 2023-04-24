78f84cb5ea9c8d2d5bc1a239729561d81119a408
tienbot1 <tien.ru@mail.ru>
2023-04-24 20:45:51 +0900

    [FIX] hide buttons if all task delete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let toDoListArr = [] //массиваная версия LS (массивLS)
+let idLast = null //последний присвоенный id
+let toDoListArr = [] //массив данных для отслеживания их кол-ва
 let list = document.querySelectorAll('.list')
 const toDoList__main = document.querySelector('.toDoList__main')
 const toDoList__bottom = document.querySelector('.toDoList__bottom')
@@ -25,68 +26,88 @@ function empty(){
     toDoList__bottom.style.display='none'
 }
 
+//отметка сделано/не сделано на сервер
+function taskCheck(iter, check){
+	fetch(`http://24api.ru/rest-todo/${iter}`, {
+		method: "PUT",
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(check)
+	})
+}
+
+//удалить данные с сервера
+function taskDel(arr){
+	const delAll = {
+        'items': arr
+    }
+
+    fetch('http://24api.ru/rest-todo/delete-items', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(delAll)
+    })
+}
+
 //отобразить задание
 function createItem(object, iter, isDone) {
     const toDoList__main = document.body.querySelector(".toDoList__main") //находим .toDoList__main
     const list = document.createElement('div') //создаем div для задания 
     list.className = "list" //добавляем ему класс .list
 
-
     const list__wrapper = document.createElement('div') //создаем обертку для задания
-    list__wrapper.className = "list__wrapper" // добавляем ей класс list__wrapper
-    const deleteItem = document.createElement('span') //создаем кнопку удаления
-    deleteItem.className = "deleteItem" // добавляем ей класс deleteItem
-    deleteItem.innerHTML = '❌' //внутри рисуем крест
+		list__wrapper.className = "list__wrapper" // добавляем ей класс list__wrapper
+		const deleteItem = document.createElement('span') //создаем кнопку удаления
+		deleteItem.className = "deleteItem" // добавляем ей класс deleteItem
+		deleteItem.innerHTML = '❌' //внутри рисуем крест
     //Слушатель 'X'
     deleteItem.addEventListener('click', () => {
             fetch(`http://24api.ru/rest-todo/${iter}`, {
                 method: 'DELETE'
             })
+			toDoListArr.pop(iter) //добавляем все id в массив для отслеживания
             deleteItem.parentElement.style.display='none'
+			if(toDoListArr.length == 0){
+				empty()
+			}
     })
 
     const checkbox = document.createElement('input') //добавляем чекбокс
-    checkbox.type = 'checkbox' //устанавливаем тип checkbox
-    checkbox.className = "customCheckbox" //добавляем класс customCheckbox
-    checkbox.id = iter//добавляем id
-    checkbox.name = iter //добавляем name
+		checkbox.type = 'checkbox' //устанавливаем тип checkbox
+		checkbox.className = "customCheckbox" //добавляем класс customCheckbox
+		checkbox.id = iter//добавляем id
+		checkbox.name = iter //добавляем name
 
     //Слушатель 'checkbox'
     checkbox.addEventListener('click', (e) => {
         let check = null
-        if (e.target.checked == true){
-            check = {"isDone": 1}
-            fetch(`http://24api.ru/rest-todo/${iter}`, {
-                method: "PUT",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(check)
-            })
-        } else if(e.target.checked == false){
-            check = {"isDone": 0}
-            fetch(`http://24api.ru/rest-todo/${iter}`, {
-                method: "PUT",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(check)
-            })
+        if (e.target.checked == true){ //если чекбокс нажат
+            check = {"isDone": 1} //isDone = 1
+			taskCheck(iter, check) //вносим изменения на сервер
+        } else { 					//иначе
+            check = {"isDone": 0}  //isDone = 0
+            taskCheck(iter, check) ////вносим изменения на сервер
         }
         checkbox.nextElementSibling.classList.toggle('close')
     })
-
+	//label
     const label = document.createElement('label') //добавляем лейбл
-    label.htmlFor = iter //добавляем ему for
-    label.innerHTML = object //пишем значение из input-а
-    if(isDone == 1) { //если в LS указано, что дело сделано - добавить классы
-        label.classList.add("close")
-        checkbox.checked = true
-    }
+		label.htmlFor = iter //добавляем ему for
+		label.innerHTML = object //пишем значение из input-а
+		if(isDone == 1) { //если указано, что дело сделано - добавить классы
+			label.classList.add("close")
+			checkbox.checked = true
+		}
     
     toDoList__main.append(list) //отображаем list
     list.append(list__wrapper, deleteItem) //отображаем все внутри list-а
     list__wrapper.append(checkbox, label) //отображаем отображаем все внутри list__wrapper-а
+	toDoListArr.push(+iter) //добавляем все id в массив
+	idLast = iter //последний iter будет idLast
+
     //отобразить центральную и нижнюю часть блока
     toDoList__main.style.display='block'
     toDoList__bottom.style.display='flex'
@@ -95,7 +116,7 @@ function createItem(object, iter, isDone) {
 // Слушатель input. Настройка работы input-а
 document.querySelector('#input').addEventListener('change', (event) => {
     const task = {
-        'id': null,
+        'id': idLast=++idLast,
         'name': input.value,
         'isDone': 0,
         "user_id": 880,
@@ -108,7 +129,9 @@ document.querySelector('#input').addEventListener('change', (event) => {
         },
         body: JSON.stringify(task)
     })
-        .then(data => createItem(task.name, task.id)) //отображаем элемент на странице;
+        // .then(data => data.json())
+        .then(data => createItem(task.name, task.id, task.isDone)) //отображаем элемент на странице;
+        // .then(data => console.log(task.name, task.id, task.isDone)) //отображаем элемент на странице;
     event.target.value = '' //опустошаем input
 })
 
@@ -119,19 +142,12 @@ document.querySelector('#deleteClose').addEventListener('click', () => {
     closeItem.forEach(el => {
         arr.push(+el.getAttribute('for'))
         el.parentElement.parentElement.style.display='none' //... скрываем все, что нашли 
+		toDoListArr = toDoListArr.filter(el => !arr.includes(el)) 
+		if(toDoListArr == 0){
+			empty()
+		}
     })
-
-    const delAll = {
-        'items': arr
-    }
-
-    fetch('http://24api.ru/rest-todo/delete-items', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(delAll)
-    })
+	taskDel(arr) //удаляем данные из сервера
 })
 
 //Слушатель 'Удалить все'
@@ -142,23 +158,12 @@ document.querySelector('#deleteAll').addEventListener('click', () => {
         arr.push(+el.children[0].children[0].id)
         el.style.display='none' //... скрываем их
     })
-
-    const delAll = {
-        'items': arr
-    }
-
-    fetch('http://24api.ru/rest-todo/delete-items', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(delAll)
-    })
-
+	taskDel(arr) //удаляем данные из сервера
     empty() //скрыть центральную и нижнюю часть блока
 })
 
 // ToDo
-// 7. Удалить нижние кнопки
-// 8. Рефакторинг кода
+// 7. Удалить нижние кнопки +
+// 8. Добавление элемента - id присваивается +
+// 8. Рефакторинг кода +
 // 9. Данные не возвращаются с сервера
