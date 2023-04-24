db2bc51ea2b02ea64a453016fd05e20780f7ab8c
tienbot1 <tien.ru@mail.ru>
2023-04-25 00:53:56 +0900

    [FIX] code refactoring

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/*  Информация о сервере
+Ссылка на сервер - https://app.swaggerhub.com/apis-docs/a-berezhkov/todo_app_sc_bc/1.0.0#/users/put_rest_user__id
+Servers изменить на http://24api.ru */
+
+const userID = 880 //id пользователя
 let idLast = null //последний присвоенный id
 let toDoListArr = [] //массив данных для отслеживания их кол-ва
 let list = document.querySelectorAll('.list')
@@ -12,7 +17,7 @@ document.querySelector('form').addEventListener("submit", (e)=>{
 
 //получение данных с сервера
 function getTask(){
-    fetch("http://24api.ru/rest-todo/items-by-id?id=880", {
+    fetch(`http://24api.ru/rest-todo/items-by-id?id=${userID}`, {
         "method": "GET"
     })
     .then(data => data.json())
@@ -63,36 +68,15 @@ function createItem(object, iter, isDone) {
 		const deleteItem = document.createElement('span') //создаем кнопку удаления
 		deleteItem.className = "deleteItem" // добавляем ей класс deleteItem
 		deleteItem.innerHTML = '❌' //внутри рисуем крест
-    //Слушатель 'X'
-    deleteItem.addEventListener('click', () => {
-            fetch(`http://24api.ru/rest-todo/${iter}`, {
-                method: 'DELETE'
-            })
-			toDoListArr.pop(iter) //добавляем все id в массив для отслеживания
-            deleteItem.parentElement.style.display='none'
-			if(toDoListArr.length == 0){
-				empty()
-			}
-    })
+        closeListener(deleteItem, iter) //Слушатель 'X'
 
     const checkbox = document.createElement('input') //добавляем чекбокс
 		checkbox.type = 'checkbox' //устанавливаем тип checkbox
 		checkbox.className = "customCheckbox" //добавляем класс customCheckbox
 		checkbox.id = iter//добавляем id
 		checkbox.name = iter //добавляем name
+        checkboxListener(checkbox, iter) //Слушатель 'checkbox'
 
-    //Слушатель 'checkbox'
-    checkbox.addEventListener('click', (e) => {
-        let check = null
-        if (e.target.checked == true){ //если чекбокс нажат
-            check = {"isDone": 1} //isDone = 1
-			taskCheck(iter, check) //вносим изменения на сервер
-        } else { 					//иначе
-            check = {"isDone": 0}  //isDone = 0
-            taskCheck(iter, check) ////вносим изменения на сервер
-        }
-        checkbox.nextElementSibling.classList.toggle('close')
-    })
 	//label
     const label = document.createElement('label') //добавляем лейбл
 		label.htmlFor = iter //добавляем ему for
@@ -116,10 +100,11 @@ function createItem(object, iter, isDone) {
 // Слушатель input. Настройка работы input-а
 document.querySelector('#input').addEventListener('change', (event) => {
     const task = {
-        'id': idLast=++idLast,
+        'id': null,
+        // 'id': idLast=++idLast,
         'name': input.value,
         'isDone': 0,
-        "user_id": 880,
+        "user_id": userID,
     }
 
     fetch("http://24api.ru/rest-todo", {
@@ -134,6 +119,35 @@ document.querySelector('#input').addEventListener('change', (event) => {
         // .then(data => console.log(task.name, task.id, task.isDone)) //отображаем элемент на странице;
     event.target.value = '' //опустошаем input
 })
+
+//Слушатель 'checkbox'
+function checkboxListener(checkbox, iter){
+    checkbox.addEventListener('click', (e) => {
+        let check = null
+        if (e.target.checked == true){ //если чекбокс нажат
+            check = {"isDone": 1} //isDone = 1
+            taskCheck(iter, check) //вносим изменения на сервер
+        } else { 					//иначе
+            check = {"isDone": 0}  //isDone = 0
+            taskCheck(iter, check) ////вносим изменения на сервер
+        }
+        checkbox.nextElementSibling.classList.toggle('close')
+    })
+}
+
+//Слушатель 'X'
+function closeListener(deleteItem, iter){
+    deleteItem.addEventListener('click', () => {
+        fetch(`http://24api.ru/rest-todo/${iter}`, {
+            method: 'DELETE'
+        })
+        toDoListArr.pop(iter) //добавляем все id в массив для отслеживания
+        deleteItem.parentElement.style.display='none'
+        if(toDoListArr.length == 0){
+            empty()
+        }
+    })
+}
 
 //Слушатель 'Удалить завершенные'
 document.querySelector('#deleteClose').addEventListener('click', () => {
@@ -161,9 +175,3 @@ document.querySelector('#deleteAll').addEventListener('click', () => {
 	taskDel(arr) //удаляем данные из сервера
     empty() //скрыть центральную и нижнюю часть блока
 })
-
-// ToDo
-// 7. Удалить нижние кнопки +
-// 8. Добавление элемента - id присваивается +
-// 8. Рефакторинг кода +
-// 9. Данные не возвращаются с сервера
